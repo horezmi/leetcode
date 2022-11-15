c4385968b7c82c02394a6652a6ec57e67f22bd47
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-11-16 00:06:17 +0300

    206. Reverse Linked List

---
+++ Linked List/reverseLinkedList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseLinkedList = function (head) {
+  let current = head;
+  let previous = null;
+
+  while (current != null) {
+    let tmp = current.next;
+    current.next = previous;
+    previous = current;
+    current = tmp;
+  }
+
+  return previous;
+};
