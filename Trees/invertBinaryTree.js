404ce97079581a5af8b888ae101a040c124cec0a
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-11-21 23:23:30 +0300

    226. Invert Binary Tree

--- Trees/invertBinaryTree.js
+++ Trees/invertBinaryTree.js
@@ -12,15 +12,15 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function (root) {
+var invertBinaryTree = function (root) {
   if (!root) return null;
 
   const tmp = root.left;
   root.left = root.right;
   root.right = tmp;
 
-  invertTree(root.left);
-  invertTree(root.right);
+  invertBinaryTree(root.left);
+  invertBinaryTree(root.right);
 
   return root;
 };
