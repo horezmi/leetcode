dc857d94eb8d4414ab4c1174e32dd2331b4b4c3a
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-12-21 23:07:09 +0300

    703. Kth Largest Element in a Stream

--- Heap and Priority Queue/KthLargestElementStream.js
+++ Heap and Priority Queue/KthLargestElementStream.js
@@ -3,16 +3,30 @@
  * @param {number} k
  * @param {number[]} nums
  */
-const KthLargestElementStream = function (k, nums) {};
+var KthLargestElementStream = function (k, nums) {
+  this.k = k;
+  this.arr = nums.sort((a, b) => b - a);
+  this.arr.length = this.arr.length > k ? k : this.arr.length;
+};
 
 /**
  * @param {number} val
  * @return {number}
  */
-KthLargestElementStream.prototype.add = function (val) {};
+KthLargestElementStream.prototype.add = function (val) {
+  const insert = () => {
+    if (this.arr[this.arr.length - 1] < val || this.arr.length < this.k) {
+      this.arr.push(val);
+      this.arr = this.arr.sort((a, b) => b - a);
+      this.arr.length = this.arr.length > this.k ? this.k : this.arr.length;
+    }
+  };
+  insert();
+  return this.arr[this.arr.length - 1];
+};
 
 /**
- * Your KthLargest object will be instantiated and called as such:
- * var obj = new KthLargest(k, nums)
+ * Your KthLargestElementStream object will be instantiated and called as such:
+ * var obj = new KthLargestElementStream(k, nums)
  * var param_1 = obj.add(val)
  */
