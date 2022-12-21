6a1a467f74c7f6470c7d15cb6632797ef0ce5cbb
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-12-21 23:05:53 +0300

    746. Min Cost Climbing Stairs

---
+++ 1-D Dynamic Prrogramming/minCostClimbingStairs.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCostClimbingStairs = function (cost) {
+  for (let i = 2; i < cost.length; i++) {
+    cost[i] = cost[i] + Math.min(cost[i - 2], cost[i - 1]);
+  }
+  return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
+};
