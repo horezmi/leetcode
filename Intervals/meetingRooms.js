869901c65de2d2a1bb30842baa36ef83b7da8e7f
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-12-22 01:02:50 +0300

    252. Meeting Rooms

--- Intervals/meetingRooms.js
+++ Intervals/meetingRooms.js
@@ -1,13 +1,10 @@
-const canAttendMeetings = (intervals) => {
+const meetingRooms = (intervals) => {
   const counters = new Set();
 
-  for (let i = 0; i < intervals.length; i += 1) {
-    for (let j = intervals[i][0]; j < intervals[i][1]; j += 1) {
-      if (counters.has(j)) {
-        return false;
-      } else {
-        counters.add(j);
-      }
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = intervals[i][0]; j < intervals[i][1]; j++) {
+      if (counters.has(j)) return false;
+      counters.add(j);
     }
   }
 
